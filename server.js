6f875520ce05afff41c54edc4cfd1a6a15067476
David Beales <david.beales@ukho.gov.uk>
2019-06-02 21:27:09 +0100

    updated api to create uuid for new records and allow updating

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var port = 8081;
 var fs = require("fs");
 var express = require('express');
 var app = express();
+const uuidv1 = require('uuid/v1');
 
 var allowCrossDomain = function (req, res, next) {
    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
@@ -15,8 +16,13 @@ app.use(allowCrossDomain);
 app.use(express.json());
 
 app.get('/', function (req, res) {
-   // First read existing users.
+
+
    fs.readFile(__dirname + "/" + "services.json", 'utf8', function (err, data) {
+      if (err) {
+         res.end();
+         return;
+      }
       var services = JSON.parse(data);
       console.log(services);
       res.setHeader('Access-Control-Allow-Origin', '*');
@@ -25,19 +31,37 @@ app.get('/', function (req, res) {
 })
 
 app.post('/', function (req, res) {
-   // First read existing users.
    fs.readFile(__dirname + "/" + "services.json", 'utf8', function (err, data) {
       console.log(req.body);
-      var services = JSON.parse(data);
+      let services = [];
+      if (data !== undefined) {
+         services = JSON.parse(data); 
+      }
+      if (req.body.id === "") {
+         service = {
+            id: uuidv1(),
+            title: req.body.title,
+            book: req.body.book,
+            who: req.body.who,
+            date: req.body.date,
+            audioId: req.body.audioId
+         };
+         services.push(service);
+      }
+      else {
+         services.forEach(service => {
+            if (service['id'] === req.body.id) {
+               service['title'] = req.body.title;
+               service['book'] = req.body.book;
+               service['who'] = req.body.who;
+               service['date'] = req.body.date;
+               service['audioId'] = req.body.audioId;
+            }
+         });
 
-      service = {
-         title: req.body.title,
-         book: req.body.book,
-         who: req.body.who,
-         date: req.body.date,
-         audioId: req.body.audioId
-      };
-      services.push(service);
+         console.log(services);
+      }
+
       fs.writeFile(__dirname + "/" + "services.json", JSON.stringify(services), function (err) {
          if (err) {
             console.log(err);
@@ -50,10 +74,6 @@ app.post('/', function (req, res) {
          console.log(response);
          res.end(JSON.stringify(response));
       });
-      // services.push(service);
-      // console.log( `\n----` );
-      // console.log( service );
-      // console.log( `\n----` );
    });
 })
 
